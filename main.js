52c38058cabd9d01e6e85d28c8b18d0bd4ba59fa
ayobeen <abdlazeezmumeen@gmail.com>
2021-04-08 15:07:16 +0100

    First Commit

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 
-//function for displaying values
+//function for getting and displaying values
 function dis(val)
 {
-document.getElementById("edu").value+=val;}
+document.getElementById("userinput").value+=val;}
 //function for evaluation
 function solve()
 {
-let x = document.getElementById("edu").value
+let x = document.getElementById("userinput").value
 let y = eval(x)
-document.getElementById("edu").value = y
+document.getElementById("userinput").value = y
 }
 //function for clearing the display
 function clr()
 {
-document.getElementById("edu").value = ""
+document.getElementById("userinput").value = ""
 }
